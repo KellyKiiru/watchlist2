956ef72a190b863c7be6833b0f7fe4e96a71ec55
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-10 00:18:15 +0300

    second commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from . import db
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -10,8 +11,6 @@ class Movie:
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
         self.vote_average = vote_average
         self.vote_count = vote_count
-
-
 
 class Review:
 
@@ -42,3 +41,21 @@ class Review:
                 response.append(review)
 
         return response
+    
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String (255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return f'User {self.username}'
+    
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic") 
+
+    def __repr__(self):
+        return f'User {self.name}'
