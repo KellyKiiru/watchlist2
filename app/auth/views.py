e8a606daf234d82fc7e782d73422f228db410d5a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-11 12:45:30 +0300

    bug:flask extension bugs

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,3 @@
-from turtle import title
 from flask import render_template,redirect,url_for, flash,request
 from . import auth
 from flask_login import login_required, login_user, logout_user
@@ -18,7 +17,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New Account"
+        #title = "New Account"
     return render_template('auth/register.html',registration_form = form, title= title)
 
 @auth.route('/login',methods=['GET','POST'])
