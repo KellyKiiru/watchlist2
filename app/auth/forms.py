db399491047e207f99befb79e279f34256f3c325
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-11 13:53:38 +0300

    Revert "bug:resolve import errors"
    This reverts commit a9f466a801fef56c729be1feab8641c3d3ac5cb9.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,7 @@
+import email
+from django.forms import ValidationError
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
